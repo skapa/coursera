f89ea4c4c7e6ff62d7ad5b9c0dc5f95cae1dd462
Druska Pavel <pavel.druska@isdd.sk>
2017-02-24 17:15:17 +0100

    idem dom

---
+++ ProgammingLanguagesB/hw5/hw5.rkt
@@ -0,0 +1,127 @@
+;; Programming Languages, Homework 5
+
+#lang racket
+(provide (all-defined-out)) ;; so we can put tests in a second file
+
+;; definition of structures for MUPL programs - Do NOT change
+(struct var  (string) #:transparent)  ;; a variable, e.g., (var "foo")
+(struct int  (num)    #:transparent)  ;; a constant number, e.g., (int 17)
+(struct add  (e1 e2)  #:transparent)  ;; add two expressions
+(struct ifgreater (e1 e2 e3 e4)    #:transparent) ;; if e1 > e2 then e3 else e4
+(struct fun  (nameopt formal body) #:transparent) ;; a recursive(?) 1-argument function
+(struct call (funexp actual)       #:transparent) ;; function call
+(struct mlet (var e body) #:transparent) ;; a local binding (let var = e in body) 
+(struct apair (e1 e2)     #:transparent) ;; make a new pair
+(struct fst  (e)    #:transparent) ;; get first part of a pair
+(struct snd  (e)    #:transparent) ;; get second part of a pair
+(struct aunit ()    #:transparent) ;; unit value -- good for ending a list
+(struct isaunit (e) #:transparent) ;; evaluate to 1 if e is unit else 0
+
+;; a closure is not in "source" programs but /is/ a MUPL value; it is what functions evaluate to
+(struct closure (env fun) #:transparent) 
+
+;; Problem 1
+
+;; CHANGE (put your solutions here)
+(define (racketlist->mupllist rl)
+  (if (null? rl)
+      (aunit)
+      (apair (car rl) (racketlist->mupllist (cdr rl)))))
+
+(define (mupllist->racketlist ml)
+  (if (aunit? ml)
+      null
+      (cons (apair-e1 ml) (mupllist->racketlist (apair-e2 ml)))))
+
+;; Problem 2
+
+;; lookup a variable in an environment
+;; Do NOT change this function
+(define (envlookup env str)
+  (cond [(null? env) (error "unbound variable during evaluation" str)]
+        [(equal? (car (car env)) str) (cdr (car env))]
+        [#t (envlookup (cdr env) str)]))
+
+;; Do NOT change the two cases given to you.  
+;; DO add more cases for other kinds of MUPL expressions.
+;; We will test eval-under-env by calling it directly even though
+;; "in real life" it would be a helper function of eval-exp.
+(define (eval-under-env e env)
+  (cond [(var? e) 
+         (envlookup env (var-string e))]
+        [(add? e) 
+         (let ([v1 (eval-under-env (add-e1 e) env)]
+               [v2 (eval-under-env (add-e2 e) env)])
+           (if (and (int? v1)
+                    (int? v2))
+               (int (+ (int-num v1) 
+                       (int-num v2)))
+               (error "MUPL addition applied to non-number")))]
+        ;; CHANGE add more cases here
+        [(int? e) e]
+        [(fun? e)
+         (closure env e)]
+        [(ifgreater? e)
+         (let ([v1 (eval-under-env (ifgreater-e1 e) env)]
+               [v2 (eval-under-env (ifgreater-e2 e) env)])
+           (if (and (int? v1)
+                    (int? v2))
+               (if (> (int-num v1) (int-num v2))
+                   (eval-under-env (ifgreater-e3 e) env)
+                   (eval-under-env (ifgreater-e4 e) env))
+               (error "MUPL comparison applied to non-number")))]
+        [(mlet? e)
+         (let ([var (mlet-var e)]
+               [v1 (eval-under-env (mlet-e e) env)])
+           (if (string? var)
+               (eval-under-env e
+;(struct mlet (var e body) #:transparent) ;; a local binding (let var = e in body)
+
+               
+        [(isaunit? e)
+         (begin
+           (print "a")
+                      
+         (if (aunit? e) 1 0))]
+        ;[(mlet? e)
+        ; (let ([
+        [(closure? e) e]
+        [#t (error (format "bad MUPL expression: ~v" e))]))
+
+;; Do NOT change
+(define (eval-exp e)
+  (eval-under-env e null))
+        
+;; Problem 3
+
+(define (ifaunit e1 e2 e3) (if (aunit? e1) e2 e3))
+
+;(define (mlet* lstlst e2)
+;  (
+
+(define (ifeq e1 e2 e3 e4) "CHANGE")
+
+;; Problem 4
+
+(define mupl-map "CHANGE")
+
+(define mupl-mapAddN 
+  (mlet "map" mupl-map
+        "CHANGE (notice map is now in MUPL scope)"))
+
+;; Challenge Problem
+
+(struct fun-challenge (nameopt formal body freevars) #:transparent) ;; a recursive(?) 1-argument function
+
+;; We will test this function directly, so it must do
+;; as described in the assignment
+(define (compute-free-vars e) "CHANGE")
+
+;; Do NOT share code with eval-under-env because that will make
+;; auto-grading and peer assessment more difficult, so
+;; copy most of your interpreter here and make minor changes
+(define (eval-under-env-c e env) "CHANGE")
+
+;; Do NOT change this
+(define (eval-exp-c e)
+  (eval-under-env-c (compute-free-vars e) null))
---
+++ ProgammingLanguagesB/hw5/hw5test.rkt
@@ -0,0 +1,71 @@
+#lang racket
+;; Programming Languages Homework 5 Simple Test
+;; Save this file to the same directory as your homework file
+;; These are basic tests. Passing these tests does not guarantee that your code will pass the actual homework grader
+
+;; Be sure to put your homework file in the same folder as this test file.
+;; Uncomment the line below and, if necessary, change the filename
+(require "hw5.rkt")
+
+(require rackunit)
+
+(define tests
+  (test-suite
+   "Sample tests for Assignment 5"
+   
+   ;; check racketlist to mupllist with normal list
+   (check-equal? (racketlist->mupllist (list (int 3) (int 4))) (apair (int 3) (apair (int 4) (aunit))) "racketlist->mupllist test")
+   (check-equal? (racketlist->mupllist (list (var "a") (var "foo"))) (apair (var "a") (apair (var "foo") (aunit))) "racketlist->mupllist test strings")
+   (check-equal? (racketlist->mupllist (list (int 3) (var "foo") (int 33))) (apair (int 3) (apair (var "foo") (apair (int 33) (aunit)))) "racketlist->mupllist test long mixed")
+   (check-equal? (racketlist->mupllist (list (int "foo") (var 4))) (apair (int "foo") (apair (var 4) (aunit))) "racketlist->mupllist test not checked")
+   
+   ;; check mupllist to racketlist with normal list
+   (check-equal? (mupllist->racketlist (apair (int 3) (apair (int 4) (aunit)))) (list (int 3) (int 4)) "racketlist->mupllist test")
+   (check-equal? (mupllist->racketlist (apair (var "a") (apair (var "b") (apair (var "c") (aunit))))) (list (var "a") (var "b") (var "c")) "racketlist->mupllist test")
+
+   ;; add test
+   (check-equal? (eval-exp (add (int 3) (int 2))) (int 5) "add test")
+   (check-equal? (eval-exp (add (int 3) (add (int 3) (int 2)))) (int 8) "add test deep")
+   
+   ;; ifaunit test
+   (check-equal? (eval-exp (ifaunit (int 1) (int 2) (int 3))) (int 3) "ifaunit test")
+   (check-equal? (eval-exp (ifaunit (aunit) (int 2) (int 3))) (int 2) "ifaunit test positive")
+   (check-equal? (eval-exp (ifaunit (var "a") (int 3) (add (int 5) (int 4)))) (int 9) "ifaunit test with add")
+
+   ;; tests if ifgreater returns (int 2)
+   (check-equal? (eval-exp (ifgreater (int 3) (int 4) (int 3) (int 2))) (int 2) "ifgreater test")
+   (check-equal? (eval-under-env (ifgreater (int 3) (int 1) (var "a") (int 3)) (list (cons "a" (int 10)))) (int 10) "ifgreater test with var")
+#|   
+   ;; mlet test
+   (check-equal? (eval-exp (mlet "x" (int 1) (add (int 5) (var "x")))) (int 6) "mlet test")
+   
+   ;; call test
+   (check-equal? (eval-exp (call (closure '() (fun #f "x" (add (var "x") (int 7)))) (int 1))) (int 8) "call test")
+   
+   ;;snd test
+   (check-equal? (eval-exp (snd (apair (int 1) (int 2)))) (int 2) "snd test")
+   
+   ;; isaunit test
+   (check-equal? (eval-exp (isaunit (closure '() (fun #f "x" (aunit))))) (int 0) "isaunit test")
+   
+   ;; mlet* test
+   (check-equal? (eval-exp (mlet* (list (cons "x" (int 10))) (var "x"))) (int 10) "mlet* test")
+   
+   ;; ifeq test
+   (check-equal? (eval-exp (ifeq (int 1) (int 2) (int 3) (int 4))) (int 4) "ifeq test")
+   
+   ;; mupl-map test
+   (check-equal? (eval-exp (call (call mupl-map (fun #f "x" (add (var "x") (int 7)))) (apair (int 1) (aunit)))) 
+                 (apair (int 8) (aunit)) "mupl-map test")
+
+   ;; problems 1, 2, and 4 combined test
+   (check-equal? (mupllist->racketlist
+   (eval-exp (call (call mupl-mapAddN (int 7))
+                   (racketlist->mupllist 
+                    (list (int 3) (int 4) (int 9)))))) (list (int 10) (int 11) (int 16)) "combined test")
+   |#   
+   ))
+
+(require rackunit/text-ui)
+;; runs the test
+(run-tests tests)
